16d9a3f8868d9949786676e7063671bff198cfae
Jordan <you@example.com>
2017-12-23 10:47:06 -0800

    Adding ability to parse a url (scraping) - this is blocked after a number of requests to the site

--- pyrealtor.py
+++ pyrealtor.py
@@ -2,6 +2,14 @@
 from bs4 import BeautifulSoup
 from collections import OrderedDict
 import os
+import requests
+import logging
+
+BASE_URL="https://www.realtor.com"
+headers = {
+    'User-Agent': 'Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/56.0.2924.87 Safari/537.36',
+}
+logging.basicConfig(level=logging.INFO)
 
 
 def parse_args():
@@ -15,17 +23,43 @@ def parse_args():
 
 def parse_path(fpath):
     if os.path.isfile(fpath):
-        yield parse_file(fpath)
+        if p:
+           yield p
     elif os.path.isdir(fpath):
-        for root, dirs, files in os.walk(fpath, topdown=False):
-            for f in files:
-                if not f.endswith('realtor.com®.html'):
-                    continue
-                yield parse_file(os.path.join(root, f))
+        for fname in os.listdir(fpath):
+            f = os.path.join(fpath, fname)
+            logging.debug(f)
+            if not os.path.isfile(f):
+                continue
+            for p in parse_file(f):
+                if p:
+                    yield p
+
 
 def parse_file(fpath):
     with open(fpath) as fp:
         soup = BeautifulSoup(fp, "lxml")
+    proplist = soup.findAll("li", {"class": "component_property-card"})
+    if not proplist:       
+        parse = parse_property(soup)
+        return [parse]
+    return iter(parse_property_url(prop) for prop in proplist)
+
+
+def parse_property_url(prop):
+    try:
+        prop_url = BASE_URL+prop["data-url"]
+        page = requests.get(prop_url, headers=headers)
+        soup = BeautifulSoup(page.content, "lxml")
+        parse =  parse_property(soup)
+        return parse
+    except (KeyError, requests.exceptions.TooManyRedirects):
+        return
+
+
+def parse_property(soup):
+    if not soup.find(id="full_address_display"):
+       return
     d = OrderedDict()
     keys = ["full_address_display", "price", "rent", "home_insurance", "hoa_fees", "property_tax"]
     for k in keys:
@@ -37,7 +71,7 @@ def parse_file(fpath):
 
 if __name__=="__main__":
     args = parse_args()
-    
+
     for p in parse_path(args.fpath):
         print(p)
 
